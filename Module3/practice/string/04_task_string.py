3dded344b5bea4f23ad18d398096f9ac5ab30092
Dors999 <89096241+Dors999@users.noreply.github.com>
2021-08-18 21:07:18 +0300

    Update 04_task_string.py

--- Module3/practice/string/04_task_string.py
+++ Module3/practice/string/04_task_string.py
@@ -4,4 +4,18 @@
 text = "Lorem ipsum dolor sit amet consectetur adipiscing elit Integer porttitor bibendum nisi ut convallis ante"
 # Примечание: для генериации текста можете воспользоваться сайтом: https://ru.lipsum.com/
 
-# TODO: your code here
+text = input("text: ")
+text=text+"."
+i=0
+k=0
+slova=0
+while text[i]!=".":
+   if text[i]!=" ":
+       k+=1
+   elif k>5:
+       slova+=1
+       k=0
+   else:
+       k=0
+   i+=1
+print("kolishestvo slov>5 simvolov",slova)
