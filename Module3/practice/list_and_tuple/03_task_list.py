058063185a6f3943886219e14943bb4655307023
Dors999 <89096241+Dors999@users.noreply.github.com>
2021-08-19 18:59:33 +0300

    Update 03_task_list.py

--- Module3/practice/list_and_tuple/03_task_list.py
+++ Module3/practice/list_and_tuple/03_task_list.py
@@ -2,3 +2,11 @@
 # Вывести на экран сумму всех элементов.
 
 # TODO: your code here
+import random
+n=int(input("Vvedite n:"))
+numbers = []
+summa=0
+for i in range(n):
+   numbers.append(random.randint(-10,10))
+   summa = summa + i
+print("summa=",summa)
