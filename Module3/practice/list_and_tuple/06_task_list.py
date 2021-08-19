985943b58f952f451a6d3f9b6eb1457784d2a8d3
Dors999 <89096241+Dors999@users.noreply.github.com>
2021-08-19 19:19:12 +0300

    Update 06_task_list.py

--- Module3/practice/list_and_tuple/06_task_list.py
+++ Module3/practice/list_and_tuple/06_task_list.py
@@ -5,3 +5,14 @@ first_number = int(input())     # Первое число
 second_number = int(input())    # Второе число
 
 # TODO: your code here
+first_number = int(input("fist="))     # Первое число
+second_number = int(input("second"))    # Второе число
+
+# TODO: your code here
+numbers=[]
+while first_number<second_number:
+    first_number+=1
+    if first_number%3==0:
+        print(first_number)
+        numbers.append(first_number)
+print(numbers)
