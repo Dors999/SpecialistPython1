167e1038c26f62ef2038324925ecdf549b1d4123
Dors999 <89096241+Dors999@users.noreply.github.com>
2021-08-19 19:06:58 +0300

    Update 04_task_list.py

--- Module3/practice/list_and_tuple/04_task_list.py
+++ Module3/practice/list_and_tuple/04_task_list.py
@@ -2,3 +2,16 @@
 # Вывести на экран сумму всех положительных элементов.
 
 # TODO: your code here
+import random
+n=int(input("Vvedite n:"))
+numbers = []
+summa=0
+for i in range(n):
+   numbers.append(random.randint(-10,10))
+i=0
+while i != n-1:
+   if numbers[i]>0:
+       summa = summa + numbers[i]
+   i+=1
+print(numbers)
+print("summa=",summa)
