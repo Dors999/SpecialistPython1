f38713f1d8448e05e4a5106ddae9f74511300553
Dors999 <89096241+Dors999@users.noreply.github.com>
2021-08-19 16:03:20 +0300

    Update 04_hw.py

--- Module3/home_work/04_hw.py
+++ Module3/home_work/04_hw.py
@@ -5,3 +5,20 @@
 # Пример:
 # Дано: [2, -5, 8, 9, -25, 25, 4]
 # Результат: [3, 5, 2]
+import random
+n=int(input("Vvedite n:"))
+numbers = []
+new_numbers=[]
+for i in range(n):
+   numbers.append(random.randint(-20,26))
+i=0
+temp=0
+print(numbers)
+while i<n:
+
+    if numbers[i]>=0:
+        if (numbers[i]**0.5)==int(numbers[i]**0.5):
+            new_numbers.append(int(numbers[i]**0.5))
+
+    i+=1
+print("new nambers=",new_numbers)
