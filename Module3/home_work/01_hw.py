35924342936524250b7c68a12febfb44566281e2
Dors999 <89096241+Dors999@users.noreply.github.com>
2021-08-19 14:27:31 +0300

    Update 01_hw.py

--- Module3/home_work/01_hw.py
+++ Module3/home_work/01_hw.py
@@ -2,8 +2,13 @@
 # Выведите все имена в одну строку через запятую
 
 names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
+i=0
+string=""
+while i!=len(names):
+    string=string+names[i]+", "
+    i+=1
+print(string[0:(len(string)-2)])
 
-# TODO: your code here
-
-# Пример вывода:
-# Иван, Ирина, Вячеслав, Василий, Петр
+#names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
+#names_string = ', '.join(names)
+#print (names_string)
