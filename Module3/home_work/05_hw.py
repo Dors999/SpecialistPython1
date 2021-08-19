61f46c7ad37ad836fb28da6beed146eb23c4bf87
Dors999 <89096241+Dors999@users.noreply.github.com>
2021-08-19 16:23:43 +0300

    Update 05_hw.py

--- Module3/home_work/05_hw.py
+++ Module3/home_work/05_hw.py
@@ -4,3 +4,20 @@
 names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
 
 # TODO: your code here
+names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
+new_names=[]
+i=0
+len_temp=0
+while i<len(names)-1:
+    print(names[i])
+    if len(names[i])>=len(names[i+1]):
+        names[i+1]=names[i]
+        del names[i]
+        i-=1
+    else:
+        del names[i]
+        i-=1
+    i+=1
+
+
+print(names)
