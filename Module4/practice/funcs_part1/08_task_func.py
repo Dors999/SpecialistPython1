8b5fb25caca058e3bcc090526187a8be401989a1
Dors999 <89096241+Dors999@users.noreply.github.com>
2021-08-20 21:49:34 +0300

    Update 08_task_func.py

--- Module4/practice/funcs_part1/08_task_func.py
+++ Module4/practice/funcs_part1/08_task_func.py
@@ -1,1 +1,15 @@
 # Напишите функцию находящую n-ое число Фибоначчи.
+def fib(n):
+    num_1=0
+    num_2=1
+    if n==1:
+        return num_1
+    elif n==2:
+        return num_2
+    elif n>2:
+        for _ in range(2,n):
+            num_buff = num_1 + num_2
+            num_1 = num_2
+            num_2 = num_buff
+        return num_buff
+print(fib(50))
