17b41551d6860d70c05190198a4ec52b0d9e487b
Dors999 <89096241+Dors999@users.noreply.github.com>
2021-08-20 20:58:02 +0300

    Update 05_task_func.py

--- Module4/practice/funcs_part1/05_task_func.py
+++ Module4/practice/funcs_part1/05_task_func.py
@@ -4,6 +4,21 @@
 # Подсказка: для нахождения площади используйте Теорему Герона
 
 # TODO: your code here
+def storona(p1,p2):
+    s=((p1[0] -p2[0] ) ** 2 + (p2[1] - p1[1]) ** 2) ** 0.5
+    return s
+def can_triangle(p1, p2, p3):
+    # TODO: your code here
+    s1 = storona(p1,p2)
+    s2 = storona(p2,p3)
+    s3 = storona(p1,p3)
+    if (s1+s2!=s3) and (s1+s3!=s2) and (s3+s2!=s1):
+        return True
+    return False
 
+
+
+# Пример вызова функции
+print(can_triangle((10, 12), (14, 18), (12, 12)))
 
 # Не забудьте протестировать вашу функцию
