f4ee99cbd596731f71ea12b5fc9a6bb6d88c8cca
Dors999 <89096241+Dors999@users.noreply.github.com>
2021-08-20 20:58:48 +0300

    Update 04_task_func.py

--- Module4/practice/funcs_part1/04_task_func.py
+++ Module4/practice/funcs_part1/04_task_func.py
@@ -1,12 +1,18 @@
 # Даны координаты трех точек p1(x1; y1) p2(x2; y2) p3(x3; y3).
 # Напишите функцию, проверябщую можно ли построить треугольник, соединив данные точки отрезками
-
+def storona(p1,p2):
+    s=((p1[0] -p2[0] ) ** 2 + (p2[1] - p1[1]) ** 2) ** 0.5
+    return s
 def can_triangle(p1, p2, p3):
     # TODO: your code here
-    pass
+    s1 = storona(p1,p2)
+    s2 = storona(p2,p3)
+    s3 = storona(p1,p3)
+    if (s1+s2!=s3) and (s1+s3!=s2) and (s3+s2!=s1):
+        return True
+    return False
+
 
 
 # Пример вызова функции
-can_triangle((10, 12), (14, 18), (12, 12))
-
-# Не забудьте протестировать вашу функцию
+print(can_triangle((10, 12), (14, 18), (12, 12)))
