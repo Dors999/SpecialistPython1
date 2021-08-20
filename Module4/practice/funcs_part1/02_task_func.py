28f180a057efa36636c2ba379711aef58791f81f
Dors999 <89096241+Dors999@users.noreply.github.com>
2021-08-20 20:27:55 +0300

    Update 02_task_func.py

--- Module4/practice/funcs_part1/02_task_func.py
+++ Module4/practice/funcs_part1/02_task_func.py
@@ -5,7 +5,33 @@
 
 def palindrome(number):
     pass
+def palindrome(number):
+    num=[]
+    print(number)
+    n1=number
+    n2=0
+    a=0
+    while n1//1>0:
+        n2=n2+(n1%10)*(10**a)
+        n1=n1//10
+        a+=1
 
+    if n2==number:
+        return "yes"
+    else:
+        return "no"
+
+
+
+
+
+
+# Тестируем функцию
+print(palindrome(3454))
+print(palindrome(3443))
+print(palindrome(1234541))
+print(palindrome(1234321))
+print(palindrome(77777))
 
 # Тестируем функцию
 print(palindrome(3454))
