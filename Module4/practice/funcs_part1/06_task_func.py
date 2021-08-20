5be5581fcf9719056cc0b398b73401bde6928a3f
Dors999 <89096241+Dors999@users.noreply.github.com>
2021-08-20 21:09:15 +0300

    Update 06_task_func.py

--- Module4/practice/funcs_part1/06_task_func.py
+++ Module4/practice/funcs_part1/06_task_func.py
@@ -1,8 +1,12 @@
 # Напишите функцию, которая проверит, что точка (x, y)
 # находится строго внутри окружности с центром в точке (xc, yc) и радиусом r:
+def storona(p1,p2):
+    s=((p1[0] -p2[0] ) ** 2 + (p2[1] - p1[1]) ** 2) ** 0.5
+    return s
 
 def point_in_circle(x, y, xc, yc, r):
-    # TODO: your code here
-    pass
+    if storona((x,y),(xc,yc))<r:
+        return True
+    return False
 
-# Не забудьте протестировать вашу функцию
+print(point_in_circle(5, 5, 2, 2, 1))
