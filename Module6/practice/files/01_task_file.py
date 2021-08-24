18d6deff28efe739cf911d4f605e46db706c9f64
Dors999 <89096241+Dors999@users.noreply.github.com>
2021-08-24 19:12:15 +0300

    Update 01_task_file.py

--- Module6/practice/files/01_task_file.py
+++ Module6/practice/files/01_task_file.py
@@ -17,3 +17,28 @@ for line in f:
 
 # Подсказка: пустые строки выглядят так "\n". Помните? Строка считывается вместе с символом переноса!
 # Применение метода "\n".rstrip() --> "" вернет вам пустую строку, строку из НУЛЯ символов.
+
+import string
+#функция удаления пробельных символов
+def remove_whitespace(in_string: str):
+    return in_string.translate(str.maketrans(dict.fromkeys(string.whitespace)))
+#открытие файла
+f = open("limericks.txt","r",encoding="utf-8")
+#печатаем файл построчно
+for line in f:
+    line = line.rstrip()
+    print(line)
+count = 0
+f.seek(0)
+#удаляем символы и считаем общее количество символов
+for line in f:
+    line = remove_whitespace(line)
+    count += len(line)
+print("Simvolov vsego=:", count)
+count = 1
+f.seek(0)
+#считаем общее количество стихотворений
+for line in f:
+    if line=="\n":
+        count+=1
+print("Stihotvoreniy vsego=",count-1)
