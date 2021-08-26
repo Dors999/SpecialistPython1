36b96634b444cd8e7acf5748eb610eecb44bbf2e
Dors999 <89096241+Dors999@users.noreply.github.com>
2021-08-26 20:20:41 +0300

    Update 03_task_struct.py

--- Module8/practice/structs/03_task_struct.py
+++ Module8/practice/structs/03_task_struct.py
@@ -4,3 +4,19 @@
 # 1. Количество элементов списка не превышающие 10
 # 2. Сумму всех положительных элементов списка
 # 3. Среднее арифметическое всех четных элементов
+import random
+
+lst=[random.randint(0,20) for _ in range(20)]
+print("spisok=",lst)
+num=[el for el in lst if el>10]
+print("kolichestvo=",len(num)+1)
+sum=0
+num=[el for el in lst if el>0]
+for el in num:
+    sum+=el
+print("summa >0=",sum)
+num=[el for el in lst if el%2==0]
+print("spisok chetnyx=",num)
+for el in num:
+    sum+=el
+print("Sr arih=",sum/(len(num)+1))
