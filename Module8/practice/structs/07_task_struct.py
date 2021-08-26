10b82dcaa517d7cd649ce42358c4c0ca22cdcec8
Dors999 <89096241+Dors999@users.noreply.github.com>
2021-08-26 20:47:47 +0300

    Update 07_task_struct.py

--- Module8/practice/structs/07_task_struct.py
+++ Module8/practice/structs/07_task_struct.py
@@ -1,2 +1,10 @@
 # Доработайте предыдущую программу так, чтобы каждый сгенерированный сотрудник
 # был уникальным(не должны повторяться комбинации Имя + Фамилия).
+import random
+
+name=["Alex","Mihail","Sergey","Ruslan"]
+fam=["Galp","Dors","Podolsk","Kamyshnikov","Ohota"]
+prof=["IT","stroitel","montagnik","rukovoditel"]
+for el in name:
+    for fa in fam:
+        print(f"1. {el} \n2. {fa} \n3. {random.randint(18,60)} \n4. {random.choice(prof)} \n5. {random.randint(18000,60000)} \n")
